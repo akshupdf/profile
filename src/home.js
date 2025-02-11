3df80a653a58771910460419f77e3a7e6ff0b35d
akshupdf <57107903+akshupdf@users.noreply.github.com>
2025-02-11 13:34:06 +0530

    Update home.js

--- src/home.js
+++ src/home.js
@@ -12,7 +12,7 @@ const Home = () => {
                 <p className="justify-center text-xl  italic p-2">" I am a B.E/I.T graduate with a passion for developing scalable web application and Working across Full stack.
 I am looking to join team with trending technology to continue to grow my skill set white contributing to the positive outcome of Firm."</p>
 <div className="m-4">
-<a href="https://drive.google.com/file/d/1rBDcpStnpyorLE0KCdQYUXnnAZfJ_cCP/view?usp=drive_link" className="relative px-5 py-2 font-medium text-white group">
+<a href="https://drive.google.com/file/d/1G2pN-Hr9UyYpdvkqUv82FdzEQUyRaHE7/view?usp=drive_link" className="relative px-5 py-2 font-medium text-white group">
 <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-purple-500 group-hover:bg-purple-700 group-hover:skew-x-12"></span>
 <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-purple-700 group-hover:bg-purple-500 group-hover:-skew-x-12"></span>
 <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
